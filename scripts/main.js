449b40f361578cb7046441fe5d4eb14c402b6271
Daniel Henderson <djhindu17@gmail.com>
2024-08-05 11:50:34 -0500

    initial commit/used bootstrap per instruction for berevity

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,26 @@
-const container = document.querySelector('#container')
+import { database } from "./database.js";
+
+const generatePikomonCards = () => {
+    const container = document.getElementById('container');
+    let pikomonHTML = '';
+
+    database.forEach((pikomon) => {
+        pikomonHTML += `
+            <div class="col-md-4 mx-auto">
+                <div class="col-12 piko-card">
+                    <img src="${pikomon.imageUrl}" class="piko-img img-fluid" alt="${pikomon.name}">
+                    <div class="piko-info">
+                        <h2 class="piko-name">${pikomon.name}</h2>
+                        <p class="info-text">Category: ${pikomon.category}</p>
+                        <p class="info-text">Abilities: ${pikomon.abilities}</p>
+                        <p class="info-text">Weakness: ${pikomon.weakness}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    container.innerHTML = pikomonHTML;
+};
+
+generatePikomonCards();
